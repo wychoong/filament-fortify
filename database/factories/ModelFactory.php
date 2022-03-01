f9186cdf0ffaf1a81e3ca4d469277450f7623755
wychoong <67364036+wychoong@users.noreply.github.com>
2022-03-01 17:36:57 +0800

    configure

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Database\Factories;
+namespace WyChoong\FilamentFortify\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,5 @@
 <?php
 
-use VendorName\Skeleton\Tests\TestCase;
+use WyChoong\FilamentFortify\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
